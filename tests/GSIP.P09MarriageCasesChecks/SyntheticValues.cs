dc3c04274adc12f276c1d11553c1dd963ecb9892
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-09 11:58:52 +0300

    Fix P09 Marriage acceptance synthetic value scope

---
+++ tests/GSIP.P09MarriageCasesChecks/SyntheticValues.cs
@@ -0,0 +1,9 @@
+global using static P09SyntheticValues;
+
+public static class P09SyntheticValues
+{
+    public const string SyntheticCivilValue = "SYNTHETIC_MARRIAGE_REQUEST";
+    public const string SyntheticApiKey = "SYNTHETIC_P09_API_KEY";
+    public const string SyntheticUsername = "SYNTHETIC_P09_USER";
+    public const string SyntheticPassword = "SYNTHETIC_P09_PASSWORD";
+}
