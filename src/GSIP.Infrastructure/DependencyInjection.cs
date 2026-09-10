f77fed8d6fdf77e677e2a500b7723b3b6a05b461
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-10 11:16:27 +0300

    hotfix: run MOJ token-flow convergence after metadata seed

--- src/GSIP.Infrastructure/DependencyInjection.cs
+++ src/GSIP.Infrastructure/DependencyInjection.cs
@@ -126,6 +126,7 @@ public static class DependencyInjection
         services.AddHostedService<IdentityDatabaseMigrationService>();
         services.AddHostedService<RbacBootstrapService>();
         services.AddHostedService<MojMetadataBootstrapService>();
+        services.AddHostedService<MojTokenFlowConvergenceService>();
         return services;
     }
 
